9c78dd6d1f22a0467a1cae0228d1214403302096
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2022-10-21 22:01:18 +0600

    Add files via upload

---
+++ Hello world & variable basic code.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First day php code</title>
+</head>
+<body>
+    <?php
+    
+    // Print function
+    echo 'hello world';
+
+    //  variable function
+    $num1=10;
+    $num2=20;
+    $sum=$num1+$num2;
+    echo $sum;
+
+    // 
+
+    $name= 'fahim faisal sifat';
+    $works='student';
+    $age=21;
+    echo 'My name is '.$name.','. 'I am a '.$works.' ' .'My age is '.$age;
+
+   $char_name='sifat';
+   $int_num=10;
+
+   echo var_dump($char_name);
+   echo var_dump($int_num);
+
+
+
+    
+    ?>
+</body>
+</html>
---
+++ php concetenation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Concetenation</title>
+</head>
+<body>
+    <?php
+     
+
+     $name= 'fahim faisal sifat';
+     $works='student';
+     $age=21;
+     
+     echo 'My name is ' .$name. '<br>' .'I am a '.$works.'<br>'.'My age is '.$age.' year old';
+    
+    ?>
+    
+</body>
+</html>
